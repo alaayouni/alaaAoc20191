def2e4fd78d999cbb5669fa85b227192baef2403
alaayouni <a.22.younis@gmail.com>
2019-09-27 09:13:55 +0300

    kjhgfdfga

--- app/src/main/java/y2019/aoc/alaa/alaaaoc2019/SafeRoudActivity.java
+++ app/src/main/java/y2019/aoc/alaa/alaaaoc2019/SafeRoudActivity.java
@@ -1,10 +1,18 @@
 package y2019.aoc.alaa.alaaaoc2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+
+
 public class SafeRoudActivity extends AppCompatActivity {
+
+
+    //Button buttonLogIn, buttonSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,24 @@ public class SafeRoudActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safe_roud);
+      //  buttonLogIn=findViewById(R.id.buttonLogIn);
+        //buttonSignUp=findViewById(R.id.buttonSignUp);
+
+        //buttonLogIn = findViewById(R.id.buttonLogIn);
+        //buttonLogIn.setOnClickListener(this);
+
+        //buttonSignUp = findViewById(R.id.buttonSignUp);
+        //buttonSignUp.setOnClickListener(this);
+
+
+
+    }
+
+   // @Override
+    public void onClick(View v) {
+
+        Intent i = new Intent(this,SignUpActivity.class);
+        startActivity(i);
     }
 }
+
