390000a6f7755c38d1b2e1e5fc1b50ec7b704353
alaayouni <a.22.younis@gmail.com>
2019-10-10 14:26:58 +0300

    kjhgfdfga

---
+++ app/src/main/java/y2019/aoc/alaa/alaaaoc2019/CameraActivity.java
@@ -0,0 +1,66 @@
+package y2019.aoc.alaa.alaaaoc2019;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+    Bitmap bitmap;
+    ImageView imageView;
+    Button btGallery, btTakePhoto;
+
+
+
+    private static final int CAMERA_REQUEST = 0;
+    private static final int SELECTIMAGE = 0;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+    public void onClick(View view) {
+
+        if (view == btTakePhoto) {
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(i, CAMERA_REQUEST);
+        } else {
+
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+
+            bitmap = (Bitmap) data.getExtras().get(&&);
+
+            imageView.setImageBitmap(bitmap);
+        }
+        else if(requestCode == SELECTIMAGE &&resultCode == Activity.RESULT_OK) {
+
+
+
+
+
+            Uri targetUri = data.getData();
+            try {
+//Decode an input stream into a bitmap.
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
