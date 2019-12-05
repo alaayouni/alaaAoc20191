4d8fb437279470e4cc200cf26d9e91e60793388a
alaayouni <a.22.younis@gmail.com>
2019-12-05 14:27:48 +0200

    059697 lululululul

--- app/src/main/java/y2019/aoc/alaa/alaaaoc2019/Item.java
+++ app/src/main/java/y2019/aoc/alaa/alaaaoc2019/Item.java
@@ -1,7 +1,15 @@
+
 package y2019.aoc.alaa.alaaaoc2019;
 
-public class Item
-{
-    public Item() {
+public class Item {
+    private String Camera;
+    private String WhatHappend;
+    private String Location;
+
+
+    public Item(String WhatHappend,String Location,String Camera) {
+        this.Camera=Camera;
+        this.Location=Location;
+        this.WhatHappend=WhatHappend;
     }
 }
