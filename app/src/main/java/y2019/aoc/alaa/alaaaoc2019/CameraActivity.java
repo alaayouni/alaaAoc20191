67ccbd733be1deea55b21b58f7d9ca61435e97af
alaayouni <a.22.younis@gmail.com>
2020-02-12 11:12:17 +0200

    m vcb5nh vkckuyy

--- app/src/main/java/y2019/aoc/alaa/alaaaoc2019/CameraActivity.java
+++ app/src/main/java/y2019/aoc/alaa/alaaaoc2019/CameraActivity.java
@@ -17,45 +17,60 @@ import java.io.FileNotFoundException;
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
     Bitmap bitmap;
     ImageView imageView;
-    Button btGallery, btTakePhoto;
+    private static final int SELECT_IMAGE = 1;
+    Button btGallery;
 
 
 
     private static final int CAMERA_REQUEST = 0;
-    private static final int SELECTIMAGE = 0;
+    Button btTakePhot;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        imageView=findViewById(R.id.ImageButton);
+        btGallery =findViewById(R.id.Gallery);
+        btTakePhot =findViewById(R.id.Camera);
+        imageView.setOnClickListener(this);
+        btTakePhot.setOnClickListener(this);
+        btGallery.setOnClickListener(this);
+
+
+
     }
 
     public void onClick(View view) {
 
-        if (view == btTakePhoto) {
+        if (view == btTakePhot) {
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(i, CAMERA_REQUEST);
         } else {
 
         }
+        Intent i =new
+        Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, SELECT_IMAGE);
+
+
     }
 
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
 
             bitmap = (Bitmap) data.getExtras().get("data");
 
             imageView.setImageBitmap(bitmap);
         }
-        else if(requestCode == SELECTIMAGE &&resultCode == Activity.RESULT_OK) {
 
 
-
-
+        else if(requestCode == SELECT_IMAGE &&resultCode == Activity.RESULT_OK) {
 
             Uri targetUri = data.getData();
             try {
-//Decode an input stream into a bitmap.
                 bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
                 imageView.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
