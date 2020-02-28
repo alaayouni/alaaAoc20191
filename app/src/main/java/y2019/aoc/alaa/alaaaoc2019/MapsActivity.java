ff0ae4282f95ec7852787c29f870cec5846ff6d7
alaayouni <a.22.younis@gmail.com>
2020-02-28 09:16:22 +0200

    LEILA REYNMEN

--- app/src/main/java/y2019/aoc/alaa/alaaaoc2019/MapsActivity.java
+++ app/src/main/java/y2019/aoc/alaa/alaaaoc2019/MapsActivity.java
@@ -1,8 +1,12 @@
 package y2019.aoc.alaa.alaaaoc2019;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,10 +14,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+     currentLocation lcation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +43,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     public void fetchLastLocation(){
-        if(ActivityCompat.checkSelfPermission(this,android)Mainfest.permission.ACCESS_FINE_LOCATION)
+        if(ActivityCompat.checkSelfPermission(this,android)Manifest.permission.ACCESS_FINE_LOCATION)
             !=PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        }
+        Task<Location> task =fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener
+                if(location!=null)
+                {
+                    currentLocation=location;
+                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()+" "+currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Inside Fetch", Toast.LENGTH_SHORT).show();
+                    SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MainActiviy.this);}
+                });
+
+
     }
+
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(getApplicationContext(), "Inside OnMapReady", Toast.LENGTH_SHORT).show();
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("i am here");
+        googleMap.addMarker(markerOptions);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
